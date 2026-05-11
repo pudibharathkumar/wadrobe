5848c28ea7ba13c33e3fce48993f08a6ca1f021c
bharathkumar <bharathkumarp1001@gmail.com>
2026-05-11 13:49:57 +0530

    Add image domains and force Vercel rebuild

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["images.unsplash.com", "ui-avatars.com", "lh3.googleusercontent.com"],
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["wadrobe-ten.vercel.app"],
+    },
+  },
 };
 
 export default nextConfig;
